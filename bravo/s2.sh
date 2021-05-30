005f41a1a9dae0151a8bfaa5bb9f56b4d15ab84e
Łukasz Sztuka <80879223+LukaszSztuka@users.noreply.github.com>
2021-05-31 01:02:57 +0200

    Dodanie komendy koniec

--- bravo/s2.sh
+++ bravo/s2.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-koniec="koniec"
 while read line; 
 do
         echo "Odczytana komenda: $line"
 
     case $line in
-        "gra")
-            bash /home/user/Pulpit/Projekt/wlasne_polecenia/gra.sh;;
         "zegar")
             bash /home/user/Pulpit/Projekt/wlasne_polecenia/zegar.sh;;
         "pokalwa")
             bash /home/user/Pulpit/Projekt/wlasne_polecenia/pokalwa.sh;;
-        "pogoda")
-            bash /home/user/Pulpit/Projekt/wlasne_polecenia/pogoda.sh;;
         "pwr")
             bash /home/user/Pulpit/Projekt/wlasne_polecenia/pwr.sh;;
         "info")
             bash /home/user/Pulpit/Projekt/wlasne_polecenia/info.sh;;
+        "koniec")
+            bash /home/user/Pulpit/Projekt/wlasne_polecenia/koniec.sh;;
         *)
             echo "#!/bin/bash" > /home/user/Pulpit/Projekt/wlasne_polecenia/bash_script.sh
             echo "$line " >> /home/user/Pulpit/Projekt/wlasne_polecenia/bash_script.sh
