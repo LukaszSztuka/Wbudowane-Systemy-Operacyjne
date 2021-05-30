0f497d1cdf5263cc77bf965c45a3e017f382e1d8
Łukasz Sztuka <80879223+LukaszSztuka@users.noreply.github.com>
2021-05-30 17:30:51 +0200

    Poprawa ścieżki do pliku png

--- bravo/wlasne_polecenia/pwr.sh
+++ bravo/wlasne_polecenia/pwr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wkhtmltoimage "https://pwr.edu.pl/studenci/kalendarz-akademicki" ./pwr.png
+wkhtmltoimage "https://pwr.edu.pl/studenci/kalendarz-akademicki" /home/user/Pulpit/Projekt/wlasne_polecenia/pwr.png
 feh /home/user/Pulpit/Projekt/wlasne_polecenia/pwr.png
